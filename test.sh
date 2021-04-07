506a1416d7c2ce4c2030ecf89c3879c3048ef668
Erik Schierboom <erik_schierboom@hotmail.com>
2021-04-07 08:44:37 +0200

    Improve naming of variable in test.sh

--- test.sh
+++ test.sh
@@ -6,15 +6,15 @@ cd "$(dirname "$0")"
 
 docker build -t test-runner .
 
-for $i in tests/*; do
-    echo "Running $i..."
+for testname in tests/*; do
+    echo "Running ${testname}..."
     docker run \
         --network none \
         --read-only \
         --rm \
-        -v "$(pwd)"/"$i":/mnt/exercism-iteration \
+        -v "$(pwd)"/"${testname}":/mnt/exercism-iteration \
         -v "$(pwd)"/output:/output \
         test-runner "fake" /mnt/exercism-iteration/ /output/
-    diff output/results.json "$i"/expected_results.json
+    diff output/results.json "${testname}"/expected_results.json
     echo "OK"
 done
