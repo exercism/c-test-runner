a97055f09bfd60a7c74d15f46d66d4bf43b1a827
Erik Schierboom <erik_schierboom@hotmail.com>
2021-04-07 08:43:53 +0200

    Run test runner using correct Docker settings

--- docker-run.sh
+++ docker-run.sh
@@ -4,4 +4,11 @@
 # $2 - A relative path to an input directory containing the submitted solution file(s) and the necessary test file(s).
 
 docker build -t test-runner .
-docker run --rm -v "$(pwd)"/"$2":/mnt/exercism-iteration -v "$(pwd)"/output:/output test-runner "$1" /mnt/exercism-iteration/ /output/
+docker run  \
+    --network none \
+    --read-only \
+    --rm \
+    --rm \
+    -v "$(pwd)"/"$2":/mnt/exercism-iteration \
+    -v "$(pwd)"/output:/output \
+    test-runner "$1" /mnt/exercism-iteration/ /output/
--- test.sh
+++ test.sh
@@ -6,9 +6,15 @@ cd "$(dirname "$0")"
 
 docker build -t test-runner .
 
-for i in tests/*; do
+for $i in tests/*; do
     echo "Running $i..."
-    docker run --rm -v "$(pwd)"/"$i":/mnt/exercism-iteration -v "$(pwd)"/output:/output test-runner "fake" /mnt/exercism-iteration/ /output/
+    docker run \
+        --network none \
+        --read-only \
+        --rm \
+        -v "$(pwd)"/"$i":/mnt/exercism-iteration \
+        -v "$(pwd)"/output:/output \
+        test-runner "fake" /mnt/exercism-iteration/ /output/
     diff output/results.json "$i"/expected_results.json
     echo "OK"
 done
